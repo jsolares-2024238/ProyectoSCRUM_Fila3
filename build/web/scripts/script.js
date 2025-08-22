c4de0e6e3234348d648edde5359e67a138fb95a5
dsebastian-2021010 <dsebastian-2021010@kinal.edu.gt>
2025-08-22 16:18:08 -0600

    update: Actualizar archivo JavaScript en ProyectoSCRUM_Fila3

--- build/web/scripts/script.js
+++ build/web/scripts/script.js
@@ -1,4 +1,4 @@
- document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function() {
         const passInput = document.querySelector('input[name="txtPass"]');
         const toggle = document.createElement("span");
         toggle.className = "toggle-password";
@@ -23,4 +23,37 @@
                 const section = document.getElementById(id);
                 section.classList.add('active');
                 section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+    
+    function showInfo(id) {
+            document.querySelectorAll('.info-content').forEach(el => el.classList.remove('active'));
+            document.getElementById(id).classList.add('active');
+    }
+    
+    /* CARRUSEL */
+    
+      const track = document.querySelector('.carousel-track');
+      const cards = Array.from(track.children);
+      const nextButton = document.querySelector('.next');
+      const prevButton = document.querySelector('.prev');
+
+      let index = 0;
+
+      function updateCarousel() {
+        const cardWidth = cards[0].getBoundingClientRect().width + 20; 
+        track.style.transform = `translateX(-${index * cardWidth}px)`;
+      }
+
+      nextButton.addEventListener('click', () => {
+        if (index < cards.length - 3) { // deja siempre 3 visibles
+          index++;
+          updateCarousel();
         }
+      });
+
+      prevButton.addEventListener('click', () => {
+        if (index > 0) {
+          index--;
+          updateCarousel();
+        }
+      });
